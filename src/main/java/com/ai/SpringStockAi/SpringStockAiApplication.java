1bf08b970e6e776d08c293205978646c74f5f05d
Naga Vineel <nagavineel@Mac.lan>
2025-05-17 16:20:50 -0500

    Initial commit

---
+++ src/main/java/com/ai/SpringStockAi/SpringStockAiApplication.java
@@ -0,0 +1,13 @@
+package com.ai.SpringStockAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStockAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStockAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/SpringStockAi/SpringStockAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.SpringStockAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStockAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
